efc389277938f835d317380339fb409ec3e13dc0
Evan <evanz1215@gmail.com>
2024-01-31 22:27:28 +0800

    Add IndexPage to home

---
+++ lib/page/index_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({super.key});
+
+  @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    // return Container();
+    return const Scaffold(
+      body: Text("Hello JD_APP"),
+    );
+  }
+}
