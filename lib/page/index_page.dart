d81f166b0d3184e3f1b6a2a300e0c748cb84acde
Evan <evanz1215@gmail.com>
2024-01-31 23:23:14 +0800

    建立Navbar UI

--- lib/page/index_page.dart
+++ lib/page/index_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:jd_app/page/home_page.dart';
+import 'package:jd_app/page/category_page.dart';
+import 'package:jd_app/page/cart_page.dart';
+import 'package:jd_app/page/user_page.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -11,8 +15,37 @@ class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
     // return Container();
-    return const Scaffold(
-      body: Text("Hello JD_APP"),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 超過3個項目時，需要設定此屬性
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "首頁",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "分類",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "購物車",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "我的",
+          ),
+        ],
+      ),
+      body: const IndexedStack(
+        index: 0,
+        children: <Widget>[
+          HomePage(),
+          CategoryPage(),
+          CartPage(),
+          UserPage(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/page/user_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({super.key});
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("我的"),
+      ),
+      body: Container(),
+    );
+  }
+}
