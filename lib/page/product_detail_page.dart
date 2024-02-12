fd8a0b3f7565cfcb310db791738d07d0cb8c2655
Evan <evanz1215@gmail.com>
2024-02-13 01:42:59 +0800

    商品詳細底部

--- lib/page/product_detail_page.dart
+++ lib/page/product_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:jd_app/provider/product_detail_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -48,7 +49,60 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             );
           }
 
-          return Container();
+          return Stack(
+            children: [
+              //主體
+
+              // 底部菜單
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            top: BorderSide(
+                                color: Color(0xffe8e8ed), width: 1))),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: InkWell(
+                            child: Container(
+                              height: 60,
+                              color: Colors.white,
+                              child: const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.shopping_cart),
+                                  Text(
+                                    "購物車",
+                                    style: TextStyle(fontSize: 13.0),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            child: InkWell(
+                          onTap: () {},
+                          child: Container(
+                              height: 60,
+                              color: const Color(0xffe93b3d),
+                              alignment: Alignment.center,
+                              child: const Text(
+                                "加入購物車",
+                                style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                        ))
+                      ],
+                    ),
+                  ))
+            ],
+          );
         }),
       ),
     );
